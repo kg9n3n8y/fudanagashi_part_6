d5da92231aa9c1fb2007b0088667cee01190832e
AmaiOmikan <64887878+kg9n3n8y@users.noreply.github.com>
2025-04-27 20:27:57 +0900

    Add files via upload

---
+++ fudalist.js
@@ -0,0 +1,17 @@
+const fudalist = [
+    // こ
+    { no:  10, kimariji: "これ", normal: "./torifuda/tori_kore.png", reverse: "./torifuda/tori_r_kore.png"},
+    { no:  24, kimariji: "この", normal: "./torifuda/tori_kono.png", reverse: "./torifuda/tori_r_kono.png"},
+    { no:  29, kimariji: "こころあ", normal: "./torifuda/tori_kokoroa.png", reverse: "./torifuda/tori_r_kokoroa.png"},
+    { no:  41, kimariji: "こい", normal: "./torifuda/tori_koi.png", reverse: "./torifuda/tori_r_koi.png"},
+    { no:  68, kimariji: "こころに", normal: "./torifuda/tori_kokoroni.png", reverse: "./torifuda/tori_r_kokoroni.png"},
+    { no:  97, kimariji: "こぬ", normal: "./torifuda/tori_konu.png", reverse: "./torifuda/tori_r_konu.png"},
+    // お
+    { no:   5, kimariji: "おく", normal: "./torifuda/tori_oku.png", reverse: "./torifuda/tori_r_oku.png"},
+    { no:  26, kimariji: "おぐ", normal: "./torifuda/tori_ogu.png", reverse: "./torifuda/tori_r_ogu.png"},
+    { no:  44, kimariji: "おおこ", normal: "./torifuda/tori_ooko.png", reverse: "./torifuda/tori_r_ooko.png"},
+    { no:  60, kimariji: "おおえ", normal: "./torifuda/tori_ooe.png", reverse: "./torifuda/tori_r_ooe.png"},
+    { no:  72, kimariji: "おと", normal: "./torifuda/tori_oto.png", reverse: "./torifuda/tori_r_oto.png"},
+    { no:  82, kimariji: "おも", normal: "./torifuda/tori_omo.png", reverse: "./torifuda/tori_r_omo.png"},
+    { no:  95, kimariji: "おおけ", normal: "./torifuda/tori_ooke.png", reverse: "./torifuda/tori_r_ooke.png"}
+]
---
+++ script.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("dblclick", function(e) { e.preventDefault(); }, { passive: false });
+
+    // 流す札の枚数をここに入力
+    const allFuda = 13;
+
+    // 札を混ぜる
+    let fudaOrder = [...fudalist];
+    fudaOrder = shuffleArray(fudaOrder)
+
+    let startTime;
+    let currentFuda = 0;
+
+    // HTML要素の取得
+    const imageElement = document.getElementById('random-image');
+    const reloadButton = document.getElementById('reload-button');
+    const kimariji = document.getElementById('kimariji');
+    const kimarijiButton = document.getElementById('kimariji-button');
+
+    // 決まり字の表示
+    kimarijiButton.addEventListener('click', function() {
+        kimariji.style.color = '#CC0000'; // 赤色に変更
+    });
+
+    // 配列をシャッフルする
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    // タイマーの停止・リセット
+    function stopTimer() {
+        const elapsedTime = Date.now() - startTime;
+        const seconds = Math.floor(elapsedTime / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        alert(`終わりです。${minutes}分${remainingSeconds}秒でした！`);
+
+        resetPage();
+    }
+
+    // 最初からボタンクリック時のイベント
+    function reloadPage(){
+        let flag = window.confirm("最初の状態に戻りますが、いいですか？");
+        if(flag) {
+            resetPage();
+        }
+    }
+
+    // 状態のリセット
+    function resetPage(){
+        imageElement.src = './torifuda/tori_0.png';
+        kimariji.textContent = '　';
+        kimariji.style.color = '#E1DAC3'; // 元の色に戻す
+        currentFuda = 0;
+        fudaOrder = shuffleArray(fudaOrder);
+    }   
+
+    // 札リストから選ばれた札を表示
+    function displayFuda(order) {
+        const fuda = fudaOrder[order];
+        const isFlipped = Math.random() < 0.5;
+        imageElement.src = isFlipped ? fuda.reverse : fuda.normal;
+        document.getElementById('kimariji').textContent = fuda.kimariji;
+    }
+
+    // 画像クリック時のイベント
+    imageElement.addEventListener('click', () => {
+        if (currentFuda === 0) {
+            startTime = Date.now();
+            displayFuda(currentFuda);
+            currentFuda++;
+        } else if (currentFuda === allFuda) {
+            stopTimer();
+        } else {
+            displayFuda(currentFuda);
+            currentFuda++;
+        }
+
+        kimariji.style.color = '#E1DAC3'; // 元の色に戻す
+    });
+
+    // 最初からボタンクリックでリロードイベント
+    reloadButton.addEventListener('click', reloadPage);
+});
